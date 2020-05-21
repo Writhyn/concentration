ee70a7f4df6b0a3b8203cc04c7ab5d5c4253e06e
writhyn <writhyn@gmail.com>
2020-05-21 19:13:37 -0400

    fixed queryselectorall

--- app.js
+++ app.js
@@ -21,51 +21,45 @@ const createGrid = () => {
         const card = document.createElement('img');
         card.setAttribute('class', 'card');
         card.setAttribute('src', 'images/card-back.png');
-        card.setAttribute('id', i)
+        card.setAttribute('id', i);
+        card.setAttribute('data-img', el.img);
+        card.setAttribute('name', el.name);
         card.addEventListener('click', flipCard);
-        card.addEventListener('click', burn);
+        // card.addEventListener('click', burn);
         cardCont.appendChild(card);
         grid.appendChild(cardCont);
     })
 }
 
 const checkMatch = () => {
-    const visibleCards = document.querySelectorAll('.card');
-    if (chosenCards[0].card === chosenCards[1].card) {
-        visibleCards[chosenCards[0].id].setAttribute('class', 'matched');
-        visibleCards[chosenCards[1].id].setAttribute('class', 'matched');
+    console.log(chosenCards);
+    if (chosenCards[0].name === chosenCards[1].name) {
+        chosenCards[0].setAttribute('class', 'matched');
+        chosenCards[1].setAttribute('class', 'matched');
     } else {
-        visibleCards[chosenCards[0].id].setAttribute('src', 'images/card-back.png');
-        visibleCards[chosenCards[1].id].setAttribute('src', 'images/card-back.png');
+        chosenCards[0].setAttribute('src', 'images/card-back.png');
+        chosenCards[1].setAttribute('src', 'images/card-back.png');
     }
     chosenCards = [];
 }
 
 let burningCards = [];
-// document.addEventListener('click', burn);
-
 function burn() {
     const num = Math.floor(Math.random() * cardsInPlay.length);
-    const burningCard = document.querySelectorAll('img')[num];
+    const burningCard = document.querySelectorAll('.card')[num];
     const flame = document.createElement('img');
     flame.setAttribute('src', 'images/fire.png');
     flame.setAttribute('class', 'fire');
-    burningCard.insertAdjacentElement('afterend', flame);
+    burningCard.insertAdjacentElement('beforebegin', flame);
     burningCards.push(burningCard.id);
 }
 
 function flipCard() {
-    const thisCard = cardsInPlay[this.id];
-    this.setAttribute('src', thisCard.img);
-    chosenCards.push({
-        card: thisCard.name,
-        id: this.id
-    });
+    this.setAttribute('src', this.dataset.img);
+    chosenCards.push(this);
     if (chosenCards.length === 2) {
-        setTimeout(checkMatch, 800);
+        setTimeout(checkMatch, 500);
     }
-    console.log(chosenCards);
-    
 }
 
 createGrid();
